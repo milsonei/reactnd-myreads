5707bd383da1f0d236a83ba9cd310722d4a360fa
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2018-12-14 13:09:19 -0200

    Componente responsável por executar a rotina que move um livro de uma prateleira para outra.

---
+++ src/components/BookShelfChanger.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import BooksUtil from '../utils/BooksUtil';
+
+class BookShelfChanger extends Component{
+    constructor(props){
+        super(props);
+        this.shelfTypes = BooksUtil.getShelfTypes();
+    }
+
+    static propTypes = {
+        onBookShelfChange: PropTypes.func.isRequired,
+        shelf: PropTypes.string.isRequired
+    }
+ 
+    shelfTypes = null;
+
+    handleChange = (e) => {    
+        this.props.onBookShelfChange(e.target.value)
+    }
+
+    render() {
+        const {bookid, shelf} = this.props;
+        return (
+            <div className="book-shelf-changer">
+                <select key={`shelf-book-${bookid}`} value={shelf} onChange={this.handleChange}>
+                    <option value="move" disabled>Move to...</option>
+                    { this.shelfTypes.map(option => (<option key={option.id} value={option.id}>{option.name}</option>)) }          
+                </select>
+            </div>
+        )
+    }
+}
+
+export default BookShelfChanger
